8f6bf0235f427870e1ad8c8d00dc2f7343184036
Aleksey Sokolov <sokolov.alexey@me.com>
2022-01-12 21:23:33 +0300

    initial commit - API

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,38 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
+  const xhr = new XMLHttpRequest();
+  xhr.responseType = 'json';
 
+  if (options.method === 'GET') {
+    let adr = `${options.url}?`;
+    for (const item in options.data) {
+      adr += `${item}=${options.data[item]}&`;
+    }
+    const url = adr.slice(0, -1);
+    try {
+      xhr.open('GET', url);
+      xhr.send();
+    } catch (err) {
+      alert(err);
+    }
+  } else {
+    const formData = new FormData();
+    for (const item in options.data) {
+      formData.append(`${item}`, `${options.data[item]}`);
+    }
+    try {
+      xhr.open(`${options.method}`, `${options.url}`);
+      xhr.send(formData);
+    } catch (err) {
+      alert(err);
+    }
+  }
+  xhr.onload = () => {
+    console.log(xhr.response);
+    options.callback(null, xhr.response);
+  };
+  xhr.onerror = () => {
+    options.callback(xhr.response.error, xhr.response);
+  };
 };
